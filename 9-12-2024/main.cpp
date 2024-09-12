a76c5dc8c4fb97613846c92f90f0435c55a0e3e2
Elygg <Eligijuskont@gmail.com>
2024-09-12 11:33:02 +0300

    antras stumimas

---
+++ 9-12-2024/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a , b , c , d, e;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    int vidurkis = (a + b + c + d + e) / 5;
+
+    cout << vidurkis << endl;
+
+    return 0;
+}
