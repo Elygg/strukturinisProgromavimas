94a7f81545bb11a820b6234cfa5091a7a87906e7
Elygg <Eligijuskont@gmail.com>
2024-10-10 11:29:21 +0300

    push

--- Antas-praktinis darbas/main.cpp
+++ Antas-praktinis darbas/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
 
+using namespace std;
+
+
+void Balsiu_palyginimas (){
+}
+
+void Maziausias_bendras_daliklis () {
+
+}
+
+void Zaidimas() {
+
+}
+
+void fizzBuzz() {
+
+}
+
+
+
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int pasirinkimas;
+
+    do {
+        cout << "Pasirinkite funkcija" << endl;
+        cin >> pasirinkimas;
+
+        switch (pasirinkimas) {
+            case 1:
+                Balsiu_palyginimas();
+            break;
+            case 2:
+                Maziausias_bendras_daliklis();
+            break;
+            case 3:
+                Zaidimas();
+            break;
+            case 4:
+                fizzBuzz();
+            break;
+            default:
+                cout << "Neteisingas pasirinkimas. Bandykite dar karta." << endl;
+        }
+    } while (pasirinkimas != 0);
     return 0;
 }
